56560b4acb69d2430e3983af06db0d3753aa5e9b
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-07-13 16:33:12 -0500

    Add project files.

---
+++ AspMvcDemoProject/Controllers/DemoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspMvcDemoProject.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return "Hello World!!!";
+            return View();
+        }
+
+        public IActionResult MyAction(int id)
+        {
+            return Ok($"Has been done successfully with id = {id}"); // 200
+        }
+
+        public IActionResult StatusCode()
+        {
+            // return BadRequest(); // 400 
+            // return Unauthorized("Message"); // 401
+            // return NotFound(); // 404
+            return new UnsupportedMediaTypeResult();
+        }
+    }
+}
